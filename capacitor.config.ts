178e0bc5b2c8ac549265336388f7ae1f83f16543
Motzz <s160419007@student.ubaya.ac.id>
2022-11-07 22:56:40 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionicBlank_160419007',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
